6213ae98e5f3f7bebe9f73e3e9b9548e3db73ea0
Krystian <74000929+pikuskrystian@users.noreply.github.com>
2021-07-15 10:04:52 +0200

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,60 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
+
+
+#include "stm32f4xx.h"
+
+
+int main(void)
+{
+
+
+	//port D
+	//12 dioda led
+
+
+
+	//przycisk
+	//PA0
+	// pord A | pin 0
+
+	HAL_Init();
+
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+
+	//dioda
+	GPIO_InitTypeDef gpio;
+	gpio.Pin = GPIO_PIN_12|GPIO_PIN_13;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Pull = GPIO_NOPULL;
+	gpio.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOD, &gpio);
+
+
+	//przycisk
+	gpio.Pin = GPIO_PIN_0;
+	gpio.Mode = GPIO_MODE_INPUT;
+	//gpio.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(GPIOA,  &gpio);
+
+
+
+	while(1)
+	{
+		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)) { // jesli przycisk jest przycisniety,
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13, GPIO_PIN_SET); // zapal diode
+				} else {
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13, GPIO_PIN_RESET);
+				}
+			}
+	}
+
